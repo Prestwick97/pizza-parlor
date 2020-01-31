d4e4e6c72c009f2a9c0d02b08d1c17c1bfa84208
Prestwick97 <prestwick97@gmail.com>
2020-01-31 11:07:27 -0800

    fix contructor, begin checkbox logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 function pizza(size, topping) {
 this.size = size,
-this.topping
+this.topping = topping
 }
 
+
+
+
+
+$(document).ready(function(){
+
+  var checkboxes = $('#"input:checkbox[name=pizzaTopping]:checked"');
+      
+  checkboxes.change(function(){
+      var countCheckedCheckboxes = checkboxes.filter(':checked').length;
+  });
+
+});
