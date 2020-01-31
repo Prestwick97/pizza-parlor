ed7d76deb66200aaf2d2fa3f0132ffdf9bdb9a87
Prestwick97 <prestwick97@gmail.com>
2020-01-31 11:52:16 -0800

    finish sizeCost prototype

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,34 @@ this.topping = topping
 
 
 $(document).ready(function(){
+  $("form#formPizza").submit(function(event){
+    event.preventDefault();
+    var checkboxes = $("input:checkbox[name=pizzaTopping]:checked");
+    var countToppings = checkboxes.filter(':checked').length;
+    
+    
+    var size = $("#pizzaSize").val();
+    var topping = parseInt(countToppings);
+    var price = 0;
+    var newPizza = new pizza(size, topping);
 
-  var checkboxes = $('#"input:checkbox[name=pizzaTopping]:checked"');
-      
-  checkboxes.change(function(){
-      var countCheckedCheckboxes = checkboxes.filter(':checked').length;
+    pizza.prototype.sizeCost = function() {
+      if(this.size == "small"){
+      price + 10;
+      }
+      else if(this.size == "medium") {
+        price + 11;
+      }
+      else if(this.size == "large") {
+        price + 12;
+      }
+      else {
+        alert("Please select a size");
+      }
+    }
+
+    pizza.prototype.toppingCost = function() {
+      price + topping;
+    }
   });
-
 });
